f0d20de36afdf287038c8c415996790b52cb26e3
Juan Alonso <juan@juanalonso.net>
2022-10-10 11:48:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@ targets=".git .tmux.conf .vim .vimrc .zshrc"
 
 setopt shwordsplit
 for f in $targets; do
+  if [[ -f ~/$f ]]; then
+    mv ~/$f ~/$f.bak
+  fi
   ln -s $(pwd)/$f ~
 done
 unsetopt shwordsplit
