dd2da98073bc03dffb807f6c274a5d266e564a86
Juan Alonso <juan@juanalonso.net>
2022-10-11 13:13:45 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,9 +13,6 @@ targets=".gitconfig .gitignore .tmux.conf .vim .vimrc .zshrc"
 
 # setopt shwordsplit
 for f in $targets; do
-  if [[ -f ~/$f ]]; then
-    mv ~/$f ~/$f.bak
-  fi
   ln -s $DOT_FILES_DIR/$f ~
 done
 # unsetopt shwordsplit
