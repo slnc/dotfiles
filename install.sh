9b691e115fa1cf60cac64eef0c28d8eaac14b4b0
Juan Alonso <juan@juanalonso.net>
2022-10-11 13:08:22 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,22 @@
 #!/bin/zsh
-targets=".git .tmux.conf .vim .vimrc .zshrc"
+#
+# How to install:
+# curl -L https://raw.githubusercontent.com/slnc/dotfiles/master/install.sh |sh
+
+
+DOT_FILES_DIR=~/files/settings/dotfiles
+
+mkdir ~/files
+git clone https://github.com/slnc/dotfiles.git $DOT_FILES_DIR
+
+targets=".gitconfig .gitignore .tmux.conf .vim .vimrc .zshrc"
 
 setopt shwordsplit
 for f in $targets; do
   if [[ -f ~/$f ]]; then
     mv ~/$f ~/$f.bak
   fi
-  ln -s $(pwd)/$f ~
+  ln -s $(pwd)/$DOT_FILES_DIR/$f ~
 done
 unsetopt shwordsplit
 
