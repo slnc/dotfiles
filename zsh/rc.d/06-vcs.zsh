37f3e12135990d65d34e74927aba6af5e1828c8a
Juan Alonso <juan@journalytic.com>
2024-05-28 11:56:39 +0200

    fixes git not ignoring gitignored untracked files

--- zsh/rc.d/06-vcs.zsh
+++ zsh/rc.d/06-vcs.zsh
@@ -12,6 +12,7 @@ precmd_vcs_info() { vcs_info }
 precmd_functions+=( precmd_vcs_info )
 setopt prompt_subst
 PS1="${PS1}\$vcs_info_msg_0_"
+
 zstyle ':vcs_info:git:*' formats '%b %u%c'
 zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
 zstyle ':vcs_info:*' check-for-changes true
@@ -21,8 +22,15 @@ zstyle ':vcs_info:git:*' formats '%K{16} %F{242}%b%F{162}%c%u%f%F{240} %f%k '
 zstyle ':vcs_info:*' enable git
 
 +vi-git-untracked() {
-  if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
-  [[ $(git ls-files --other --directory --exclude-standard | sed q | wc -l | tr -d ' ') == 1 ]] ; then
-  hook_com[unstaged]+='?%f'
-fi
+  if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]]; then
+    local untracked_files
+    untracked_files=$(git ls-files --other --directory --exclude-standard)
+    for file in $untracked_files; do
+      if [[ -z $(git check-ignore -v $file) ]]; then
+        hook_com[unstaged]+='?%f'
+        break
+      fi
+    done
+  fi
 }
+
