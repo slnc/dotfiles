c391c6be5c3eee442a4e2ec5f9e1a1d311e048bf
slnc <juan@juan.al>
2024-07-07 19:19:18 +0200

    Make install.sh run on /bin/sh

--- install.sh
+++ install.sh
@@ -1,30 +1,23 @@
-#!/usr/bin/env zsh
-#
-# - curl -L https://raw.githubusercontent.com/slnc/dotfiles/master/install.sh | zsh
+#!/bin/sh
+# - curl -L https://raw.githubusercontent.com/slnc/dotfiles/master/install.sh | sh
 set -uex
 
 DOTFILES_DIR=~/.dotfiles
-git clone https://github.com/slnc/dotfiles.git ${DOTFILES_DIR}
+git clone https://github.com/slnc/dotfiles.git "${DOTFILES_DIR}"
 
-if command -v apt-get &> /dev/null; then
+if command -v apt-get > /dev/null 2>&1; then
   curl https://pyenv.run | bash
-  apt-get update && apt-get install -y silversearcher-ag
-elif command -v brew &> /dev/null; then
-  brew install pyenv
+  apt-get update && apt-get install -y zsh vim silversearcher-ag
+elif command -v brew > /dev/null 2>&1; then
+  brew install pyenv vim
 else
   echo "Unable to determine the package manager. Install pyenv manually."
 fi
 
-targets=(
-  .vimrc
-  .zshenv
-)
-
-for f in "${targets[@]}"; do
+for f in ".vimrc .zshenv"; do
   ln -s "${DOTFILES_DIR}/${f}" ~
 done
 
 vim +PlugInstall +qall
-
 curl -L https://iterm2.com/shell_integration/install_shell_integration.sh | bash
 chsh -s /bin/zsh
