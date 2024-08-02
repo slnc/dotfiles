a9b26bd5ddff612c992514f78f2c3c88c1020048
Juan Alonso <juan@journalytic.com>
2024-08-02 16:48:28 +0200

    ignore node_modules

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,3 +1,11 @@
+require('telescope').setup{
+        defaults = {
+                file_ignore_patterns = {
+                        "node_modules"
+                }
+        }
+}
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {}) -- project files
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
