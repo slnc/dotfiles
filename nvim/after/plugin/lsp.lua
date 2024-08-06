0cc11809b6359c6fc0faa161104a2ef8ba333fd4
slnc <juan@juan.al>
2024-08-06 09:38:29 +0200

    various fixes

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,71 +1,98 @@
 local lsp = require('lsp-zero')
 
 -- lsp.ensure_installed("recommended")
--- 
+--
 -- lsp.ensure_installed({
 -- })
+--
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-y>'] = cmp.mapping.confirm({ select = true }),
-	['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(),
 })
 
 
 lsp.on_attach(function(client, bufnr)
-	-- see :help lsp-zero-keybindings
-	-- to learn the available actions
-	-- lsp.default_keymaps({buffer = bufnr})
-	local opts = { buffer = bufnr, remap = false }
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-	vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    -- lsp.default_keymaps({buffer = bufnr})
+    local opts = { buffer = bufnr, remap = false }
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
 end)
+
+-- if n LSPs active, order is non-deterministic:
+-- https://lsp-zero.netlify.app/v3.x/language-server-configuration.html#enable-format-on-save
+lsp.on_attach(function(client, bufnr)
+    lsp.default_keymaps({ buffer = bufnr })
+    lsp.buffer_autoformat()
+end)
+
+-- lsp.format_on_save({
+--         format_opts = {
+--                 async = false,
+--                 timeout_ms = 10000,
+--         },
+--         servers = {
+--                 ['lua_ls'] = { 'lua' },
+--                 ['tsserver'] = { 'javascript', 'typescript' },
+--         }
+-- })
 
 -- here you can setup the language servers
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {
-'tsserver',
-'eslint',
-},
-  handlers = {
-    function(server_name)
-      require('lspconfig')[server_name].setup({})
-    end,
-  },
+    ensure_installed = {
+        'lua_ls',
+        'tsserver',
+        'eslint',
+    },
+    handlers = {
+        function(server_name)
+            require('lspconfig')[server_name].setup({})
+        end,
+    },
 })
 
 local lspconfig = require('lspconfig')
-lspconfig.pylsp.setup{
-        filetypes = { "python" },
-  settings = {
-    pylsp = {
-      plugins = {
-        pycodestyle = {enabled = false},
-        pylint = {enabled = false},
-        rope_completion = {enabled = true},
-      }
+lspconfig.pylsp.setup {
+    filetypes = { "python" },
+    settings = {
+        pylsp = {
+            plugins = {
+                pycodestyle = { enabled = false },
+                pylint = { enabled = false },
+                rope_completion = { enabled = true },
+            }
+        }
     }
-  }
 }
 
--- lspconfig.lua_ls.setup {
---         settings = {
---                 Lua = {
---                         diagnostics = {
---                                 globals = {'vim'},
---                         }
---                 },
---         },
--- }
+lspconfig.lua_ls.setup {
+    settings = {
+        Lua = {
+            format = {
+                enable = true,
+                -- Put format options here
+                -- NOTE: the value should be String!
+                -- DO NOT SUBMIT: not effective
+                defaultConfig = {
+                    indent_style = "space",
+                    indent_size = "2",
+                }
+            },
+        },
+    },
+}
--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -2,12 +2,28 @@ require('telescope').setup{
         defaults = {
                 file_ignore_patterns = {
                         "node_modules"
-                }
-        }
+                },
+                pickers = {
+
+
+
+        },
+        },
+        vimgrep_arguments = {
+      'rg',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--smart-case',
+      '--hidden',
+    },
 }
 
+-- require("telescope.builtin").find_files { find_command = { "rg", "--hidden" } }
+
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {}) -- project files
+vim.keymap.set('n', '<leader>pf', function() builtin.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }}) end, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
