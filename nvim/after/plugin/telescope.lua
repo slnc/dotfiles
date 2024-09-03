4bfa6a0f0b496ed7198797b7e30a9fa357ab0876
slnc <juan@juan.al>
2024-09-03 18:52:04 +0200

    formatting changes

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,44 +1,45 @@
 require('telescope').setup {
-        defaults = {
-                file_ignore_patterns = {
-                        "node_modules"
-                },
-                pickers = {
+  defaults = {
+    file_ignore_patterns = {
+      "node_modules"
+    },
+    pickers = {
 
 
 
-                },
-        },
-        vimgrep_arguments = {
-                'rg',
-                '--color=never',
-                '--no-heading',
-                '--with-filename',
-                '--line-number',
-                '--smart-case',
-                '--hidden',
-        },
-        pickers = {
-                find_files = {
-                        hidden = true
-                },
-                grep_string = {
-                        additional_args = { "--hidden", "--smart-case", "-f", "!.git" }
-                },
-                live_grep = {
-                        additional_args = { "--hidden", "--smart-case" }
-                },
-        },
+    },
+  },
+  vimgrep_arguments = {
+    'rg',
+    '--color=never',
+    '--no-heading',
+    '--with-filename',
+    '--line-number',
+    '--smart-case',
+    '--hidden',
+  },
+  pickers = {
+    find_files = {
+      hidden = true
+    },
+    grep_string = {
+      additional_args = { "--hidden", "--smart-case", "-f", "!.git" }
+    },
+    live_grep = {
+      additional_args = { "--hidden", "--smart-case" }
+    },
+  },
 }
 
 local builtin = require('telescope.builtin')
 local find_cmd = { 'rg', '--files', '--hidden', '-g', '!.git' }
 vim.keymap.set('n', '<leader>f',
-        function() builtin.find_files({ find_command = find_cmd }) end, {})
+  function() builtin.find_files({ find_command = find_cmd }) end, {})
 vim.keymap.set("n", "<leader>of", builtin.oldfiles, {})
 vim.keymap.set("n", "<leader>lg", builtin.live_grep, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set("x", "<leader>p", [["_dP]]) -- del to blackhole register, then paste default register
 vim.keymap.set('n', '<leader>gs', function()
-        builtin.grep_string({ search = vim.fn.input("Grep > ") });
+  builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end, { desc = "grep_string" })
 -- require("telescope.builtin").find_files { find_command = { "rg", "--hidden" } }
