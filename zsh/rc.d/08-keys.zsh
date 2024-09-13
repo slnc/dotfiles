98a33e1337d199f5fa8d7fa03f0604cb640d1ad4
slnc <juan@juan.al>
2024-09-13 11:42:37 +0200

    Fix Alt+left/right not moving cursor by cursor

--- zsh/rc.d/08-keys.zsh
+++ zsh/rc.d/08-keys.zsh
@@ -1,3 +1,6 @@
 bindkey -e  # Make ^A/^E go to beginning/end of line
 bindkey \^U backward-kill-line
 bindkey '^R' history-incremental-pattern-search-backward
+
+bindkey "^[[1;3C" forward-word
+bindkey "^[[1;3D" backward-word
