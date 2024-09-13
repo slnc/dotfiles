e913708efca2d29b56e8edc7e60ccbbeafca8c5a
slnc <juan@juan.al>
2024-09-13 10:08:58 +0200

    lazy-load obsidian outside of drivers

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -1,7 +1,10 @@
+local hostname = io.popen("hostname"):read("*l")
+local should_be_lazy = hostname ~= "mbpro2019" and hostname ~= "bindan"
+
 return {
   "epwalsh/obsidian.nvim",
   version = "*", -- recommended, use latest release instead of latest commit
-  lazy = false,
+  lazy = should_be_lazy,
   ft = "markdown",
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
   -- event = {
