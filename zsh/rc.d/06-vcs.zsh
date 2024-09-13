90a2e59882fcca351d80ade3ca3b4d14640d3bff
slnc <juan@juan.al>
2024-09-13 11:14:30 +0200

    Move to gitstatus (>10x faster)

--- zsh/rc.d/06-vcs.zsh
+++ zsh/rc.d/06-vcs.zsh
@@ -7,27 +7,4 @@ autoload -Uz compinit
 compinit
 # End of lines added by compinstall
 
-autoload -Uz vcs_info
-precmd_vcs_info() { vcs_info }
-precmd_functions+=( precmd_vcs_info )
 setopt prompt_subst
-# Full prompt overriden later. Leaving this here for educational purposes.
-PS1="${PS1}\$vcs_info_msg_0_"
-
-zstyle ':vcs_info:git:*' formats '%b %u%c'
-zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
-zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:*' unstagedstr '!'
-zstyle ':vcs_info:*' stagedstr '+'
-zstyle ':vcs_info:git:*' formats '%K{16} %F{242}󰘬 %b%F{162}%c%u%f%F{240} %f%k '
-zstyle ':vcs_info:*' enable git
-
-+vi-git-untracked() {
-  if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]]; then
-    local untracked_count
-    untracked_count=$(git status --porcelain 2>/dev/null | grep '^??' | wc -l)
-    if [[ $untracked_count -gt 0 ]]; then
-      hook_com[unstaged]+='?%f'
-    fi
-  fi
-}
