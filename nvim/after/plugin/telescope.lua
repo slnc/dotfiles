5ac2f17bd0715eeb75169399cb578576141d2edb
slnc <juan@juan.al>
2024-09-14 17:31:50 +0200

    backup

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -23,7 +23,7 @@ require('telescope').setup {
       hidden = true
     },
     grep_string = {
-      additional_args = { "--hidden", "--smart-case", "-f", "!.git" }
+      additional_args = { "--hidden", "--smart-case", "-f", "!.git", "-f", "!.trash" }
     },
     live_grep = {
       additional_args = { "--hidden", "--smart-case" }
@@ -32,7 +32,7 @@ require('telescope').setup {
 }
 
 local builtin = require('telescope.builtin')
-local find_cmd = { 'rg', '--files', '--hidden', '-g', '!.git' }
+local find_cmd = { 'rg', '--files', '--hidden', '-g', '!.git', '-g', '!.trash' }
 vim.keymap.set('n', '<leader>f',
   function() builtin.find_files({ find_command = find_cmd }) end, {})
 vim.keymap.set("n", "<leader>of", builtin.oldfiles, {})
