d6eff73d9fe1802f16a4540138fb39a297c0a33b
slnc <juan@juanalonso.net>
2024-10-09 14:04:20 +0200

    add stopwatch timer

--- scripts/update_wallpaper.py
+++ scripts/update_wallpaper.py
@@ -1,31 +1,36 @@
 import argparse
 import json
 import os
-import os
 import random
 import subprocess
 
 from pathlib import Path
 from PIL import Image
 
+
 def get_images(directory):
-    image_extensions = {'.png', '.jpg', '.jpeg'}
-    return [f for f in os.listdir(directory) if Path(f).suffix.lower() in image_extensions]
+    image_extensions = {".png", ".jpg", ".jpeg"}
+    return [
+        f for f in os.listdir(directory) if Path(f).suffix.lower() in image_extensions
+    ]
+
 
 def read_history(history_file):
     if not os.path.exists(history_file):
         return set()
-    with open(history_file, 'r') as file:
+    with open(history_file, "r") as file:
         return set(json.load(file))
+
 
 def write_history(history_file, history):
     os.makedirs(os.path.dirname(history_file), 644, True)
-    with open(history_file, 'w') as file:
+    with open(history_file, "w") as file:
         json.dump(list(history), file)
 
+
 def choose_image(images, history):
-    print(images)
-    print(history)
+    # print(images)
+    # print(history)
     available_images = list(set(images) - history)
     if not available_images:
         return None
@@ -36,17 +41,28 @@ def convert_and_resize_image(input_path, output_path, size=(3840, 2160)):
     """Convert an image to PNG format and resize it."""
     with Image.open(input_path) as img:
         # Convert to RGB if not already in RGB mode
-        if img.mode != 'RGB':
-            img = img.convert('RGB')
+        if img.mode != "RGB":
+            img = img.convert("RGB")
         # Resize image
         img = img.resize(size, Image.LANCZOS)
         # Save the image as PNG
-        img.save(output_path, format='PNG')
+        img.save(output_path, format="PNG")
 
 
 def restart_feh():
-    subprocess.run(['pkill', '-f', 'feh'])
-    subprocess.run(['feh', '--bg-scale', '--zoom', 'fill', '~/files/wallpapers/today.png'])
+    subprocess.run(["pkill", "-f", "feh"])
+    subprocess.run(
+        [
+            "feh",
+            "--bg-scale",
+            "--zoom",
+            "fill",
+            os.path.expanduser("~/files/wallpapers/today.png"),
+        ],
+        env={"DISPLAY": ":1", **os.environ},
+        check=True,
+    )
+
 
 def main(directory, history_file):
     directory = os.path.abspath(directory)
@@ -64,7 +80,7 @@ def main(directory, history_file):
     if not chosen_image:
         raise RuntimeError("No images available to choose from.")
 
-    final_image_path = os.path.join(directory, 'today.png')
+    final_image_path = os.path.join(directory, "today.png")
 
     chosen_image_path = os.path.join(directory, chosen_image)
     convert_and_resize_image(chosen_image_path, final_image_path)
@@ -73,13 +89,22 @@ def main(directory, history_file):
     history.add(chosen_image)
     write_history(history_file, history)
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Randomly choose an image and create a symlink.')
-    parser.add_argument('-d', '--directory', type=str, help='The directory containing images.')
-    parser.add_argument('-f', '--history_file', type=str, help='The file to store the history of chosen images.')
+    parser = argparse.ArgumentParser(
+        description="Randomly choose an image and create a symlink."
+    )
+    parser.add_argument(
+        "-d", "--directory", type=str, help="The directory containing images."
+    )
+    parser.add_argument(
+        "-f",
+        "--history_file",
+        type=str,
+        help="The file to store the history of chosen images.",
+    )
     # /home/slnc/files/wallpapers
     # ~/.config/local/.update_wallpaper_history
 
     args = parser.parse_args()
     main(args.directory, args.history_file)
-
