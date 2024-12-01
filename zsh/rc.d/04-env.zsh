8961f563fa6768bca566d73a26f8e6221159c531
slnc <juan@juanalonso.net>
2024-12-01 10:01:30 +0100

    speed up prompt on mac

--- zsh/rc.d/04-env.zsh
+++ zsh/rc.d/04-env.zsh
@@ -36,13 +36,44 @@ fpath=(
 
 autoload -U $fpath[1]/*(.:t)
 
+# This version doesn't rehash on every keystroke. This removes the ~1s delay on mac.
+# However, with this we now need to manually clear caches with aliases around pip/pip3
+# and remember to call this command after installing new py versions.
 export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-if which pyenv-virtualenv-init > /dev/null; then
-  eval "$(pyenv virtualenv-init -)";
-fi
+eval "$(pyenv init - --no-rehash zsh)"
+[[ -f "$PYENV_ROOT/plugins/pyenv-virtualenv/bin/pyenv-virtualenv-init" ]] && \
+    eval "$(pyenv virtualenv-init - --no-rehash zsh)"
+[[ -f "$HOME/.rbenv/bin/rbenv" ]] && \
+    eval "$(rbenv init - --no-rehash zsh)"
 
-if which rbenv > /dev/null; then
-  eval "$(rbenv init - --no-rehash zsh)"
-fi
+pip() {
+  command pip "$@"
+  local ret=$?
+  if [[ "$1" == "install" ]] || [[ "$1" == "uninstall" ]]; then
+    echo "Running pyenv rehash..."
+    pyenv rehash
+  fi
+  return $ret
+}
+
+pip3() {
+  command pip3 "$@"
+  local ret=$?
+  if [[ "$1" == "install" ]] || [[ "$1" == "uninstall" ]]; then
+    echo "Running pyenv rehash..."
+    pyenv rehash
+  fi
+  return $ret
+}
+
+# export PYENV_ROOT="$HOME/.pyenv"
+# [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+# eval "$(pyenv init -)"
+# if which pyenv-virtualenv-init > /dev/null; then
+#   eval "$(pyenv virtualenv-init -)";
+# fi
+#
+# if which rbenv > /dev/null; then
+#   eval "$(rbenv init - --no-rehash zsh)"
+# fi
