0e55fff20851b6bc24337070e0382383d6abf600
slnc <juan@juanalonso.com>
2026-01-14 20:36:07 +0100

    backup

--- nvim/after/ftplugin/make.lua
+++ nvim/after/ftplugin/make.lua
@@ -1,5 +1,11 @@
 -- Use tabs instead of spaces in Makefiles
-vim.opt_local.expandtab = false
-vim.opt_local.tabstop = 4
-vim.opt_local.shiftwidth = 4
-vim.opt_local.softtabstop = 0
+-- Using BufEnter autocmd to override editorconfig (which runs on BufReadPost)
+vim.api.nvim_create_autocmd("BufEnter", {
+  buffer = 0,
+  callback = function()
+    vim.opt_local.expandtab = false
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 0
+  end,
+})
